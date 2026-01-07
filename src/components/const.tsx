73417de545741dba9044329e4fd31645f4535acb
ahmadaqib <ahmadaqibsunu@gmail.com>
2026-01-07 21:21:00 +0800

    7.1.26

--- src/components/const.tsx
+++ src/components/const.tsx
@@ -2,7 +2,7 @@ export interface Project {
   id: number;
   title: string;
   description: string;
-  image: string;
+  image?: string;
   images?: string[];
   technologies: string[];
   link?: string;
